398297fe45b49fb93e0257902cfe15825938edc9
ana-sifuentes <76788108+ana-sifuentes@users.noreply.github.com>
2021-05-09 23:48:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
-
-var character = doucment.getElementByID("character");
-
+  
+var character = document.getElementById("character");
 var block = document.getElementById("block");
 
+var counter=0;
 
 function jump(){
-  
-  if(character.classList != "animate"){
+    if(character.classList == "animate"){return}
     character.classList.add("animate");
-  }
-  
-  setTimeout(function(){
-    character.classList.remove("animate");
-  },500);
-  
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },300);
 }
-
-
-
-var checkDead = setInterval(function(){
-  var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));  
-  var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));  
-  
-  if(blockLeft < 40 && blockLeft > 0 && characterTop>=300){
-   block.style.animation ="none";
-    block.style.display = "none";
-   alert("you lose :c "); 
-  }
-},10);
-
-
-  function showAlert() {
-    alert ("Hello world!");
-  }
+var checkDead = setInterval(function() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+        block.style.animation = "none";
+        alert("Game Over. score: "+Math.floor(counter/100));
+        counter=0;
+        block.style.animation = "block 1s infinite linear";
+    }else{
+        counter++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+    }
+}, 10)
