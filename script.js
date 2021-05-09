9f52c6159763474ba541f2576372ed5e4ff62f38
ana-sifuentes <76788108+ana-sifuentes@users.noreply.github.com>
2021-05-09 23:42:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -18,25 +18,11 @@ function jump(){
 
 
 
-
-document.body.onkeyup = function(e){
-    if(e.keyCode == 32){
-      alert("clicked space bar");
-       jump();
-    }
-}
-
-
-
-
-
-
-
 var checkDead = setInterval(function(){
   var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));  
   var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));  
   
-  if(blockLeft < 20 && blockLeft > 0 && characterTop>=130){
+  if(blockLeft < 40 && blockLeft > 0 && characterTop>=300){
    block.style.animation ="none";
     block.style.display = "none";
    alert("you lose :c "); 
