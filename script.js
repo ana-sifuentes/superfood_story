aef643f0bbc124387ff6da818d0bace88b9a72ed
ana-sifuentes <76788108+ana-sifuentes@users.noreply.github.com>
2021-05-10 18:29:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ function jump(){
 var checkDead = setInterval(function() {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<30 && blockLeft>-20 && characterTop>=100){
+    if(blockLeft<30 && blockLeft>-20 && characterTop>=185){
         block.style.animation = "none";
         alert("Game Over. score: "+Math.floor(counter/100));
         counter=0;
